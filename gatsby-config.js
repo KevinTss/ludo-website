0505a14424ee611604e7746de37adff16d5de29b
KevinTss <kevin.tassi@gmail.com>
2020-11-23 22:02:24 +0100

    Add netlify CMS test

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,19 @@
 const path = require(`path`)
 
 module.exports = {
-	pathPrefix: "/screen-side",
-	plugins: [
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: path.join(__dirname, `src`, `assets`, `images`),
-			},
-		},
-		`gatsby-plugin-sharp`,
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-styled-components`,
-	],
+  pathPrefix: "/screen-side",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `assets`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify-cms`,
+  ],
 }
