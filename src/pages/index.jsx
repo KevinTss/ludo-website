ea49ab57897f3b75382f1ebf6dfe7b8da98e40aa
KevinTss <kevin.tassi@gmail.com>
2021-11-28 14:55:24 +0100

    Display products

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Intro from "../components/sections/intro"
+import products from "../../_content/products.json"
 
 const HomePage = () => (
   <Layout>
     <Intro />
+    {products.items.map(item => (
+      <p>{`Product: ${item.name} - ${item.price}euro`}</p>
+    ))}
   </Layout>
 )
 
